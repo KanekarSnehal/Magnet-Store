e67f213acec4424fbfc261892181e7ac39c97844
KanekarSnehal <snehalkanekar43@gmail.com>
2022-05-28 16:21:31 +0530

    removed auth exports

--- src/reducer/index.js
+++ src/reducer/index.js
@@ -1,6 +1,4 @@
 export { FilterReducer } from "./filterReducer";
-export { AuthReducer } from "./AuthReducer";
-export { authActionsConstants } from "./authActionsConstant";
 export { UserDataReducer } from "./userDataReducer";
 export {
   wishlistConstants,
