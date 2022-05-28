be5e662e3b4323e834c5c31a1693502c95e61a70
KanekarSnehal <snehalkanekar43@gmail.com>
2022-05-28 16:16:28 +0530

    fixed authToken name

--- src/services/helpers.js
+++ src/services/helpers.js
@@ -1,6 +1,7 @@
 export const getConfig = () => ({
-  headers: { authorization: localStorage.getItem("token") },
+  headers: { authorization: localStorage.getItem("magnetStoreToken") },
 });
 
 export const cartUrl = "/api/user/cart";
 export const wishlistUrl = "/api/user/wishlist";
+export const addressUrl = "/api/user/address";
