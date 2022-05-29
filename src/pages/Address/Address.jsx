4279252723a84a5222838728eac03e68f008eb2e
KanekarSnehal <snehalkanekar43@gmail.com>
2022-05-29 13:26:53 +0530

    Added address component

--- src/pages/Address/Address.jsx
+++ src/pages/Address/Address.jsx
@@ -1,5 +1,28 @@
 import React from "react";
+import "./address.css";
+import { useAddress, useUserData } from "../../context";
+import { AddressCard, AddressForm } from "../../components";
 
 export const Address = () => {
-  return <div>Address</div>;
+  const {
+    userState: {
+      userAddress: { addresses },
+    },
+  } = useUserData();
+  const { showAddressModal, setShowAddressModal, editAddress, setEditAddress } =
+    useAddress();
+
+  return (
+    <div className="address-page-container">
+      {addresses &&
+        addresses.map((address) => <AddressCard address={address} />)}
+      <button
+        className="btn primary-btn"
+        onClick={() => setShowAddressModal(true)}
+      >
+        + ADD ADDRESS
+      </button>
+      {showAddressModal && <AddressForm />}
+    </div>
+  );
 };
