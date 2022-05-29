b6a6fe8c92a7ed7b36ce1310a1482476f8fc3877
KanekarSnehal <snehalkanekar43@gmail.com>
2022-05-29 17:26:04 +0530

    added basic layout

--- src/pages/Checkout/Checkout.jsx
+++ src/pages/Checkout/Checkout.jsx
@@ -1,5 +1,26 @@
 import React from "react";
+import { Header } from "../../components";
+import { useUserData } from "../../context";
+import "./checkout.css";
 
 export const Checkout = () => {
-  return <div>Checkout</div>;
+  const {
+    userState: {
+      userAddress: { addresses },
+    },
+  } = useUserData();
+  return (
+    <>
+      <Header />
+      <div className="checkout-container">
+        <div className="address-page-container">
+          {addresses &&
+            addresses.map((address) => (
+              <AddressCard address={address} key={address._id} />
+            ))}
+        </div>
+        <div>bill</div>
+      </div>
+    </>
+  );
 };
